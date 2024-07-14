0594bcdd3d465b96ccf3039d77679986d00765c5
VindiPerera021 <vindiperera0221@gmail.com>
2024-07-14 17:36:37 +0530

    Add files via upload
    Selection Sort Ascending

---
+++ Selection asc.py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+    
+    for i in range(len(arr)):
+       
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+    return arr
+if __name__ == "__main__":
+    arr = [64, 25, 12, 22, 11]
+    print("Original array:", arr)
+    sorted_arr = selection_sort(arr)
+    print("Sorted array:", sorted_arr)
